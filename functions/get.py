f1071260facec616ca9d13e1bfa4d5d865a5f791
Robert Roman <robertroman7@gmail.com>
2021-04-03 18:13:19 -0500

    Added get win rate

--- functions/get.py
+++ functions/get.py
@@ -1,9 +1,5 @@
-from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from termcolor import colored
@@ -12,10 +8,10 @@ from database.profit import profits
 from functions.webdriver import driver
 
 
-def net_value(count, wait):
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@class='additional_percent_value']")))
+def net_profit(count, wait):
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
     try:
-        time.sleep(1)
+        time.sleep(.5)
         check = driver.find_elements_by_class_name("additional_percent_value")[0]
         check.find_element_by_xpath('./span[contains(@class, "neg")]')
         negative = True
@@ -34,4 +30,28 @@ def net_value(count, wait):
         profits.update({count: net_value})
         positive_color = {count: net_value}
         print(colored(f'{positive_color}', 'green'))
-    return net_value
+    return net_profit
+
+
+def win_rate(count, wait):
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
+    try:
+        win_rate = driver.find_elements_by_class_name("additional_percent_value")[1]
+        win_rate.find_element_by_xpath('./span[contains(@class, "neg")]')
+        negative = True
+    except NoSuchElementException:
+        negative = False
+
+    if negative:
+        win_rate = driver.find_elements_by_class_name("additional_percent_value")[1].text.split(" %")
+        net_value = float(win_rate[0])
+        profits.update({count: -net_value})
+        negative_color = {count: net_value}
+        print(colored(f'{negative_color}', 'red'))
+    else:
+        win_rate = driver.find_elements_by_class_name("additional_percent_value")[1].text.split(" %")
+        net_value = float(win_rate[0])
+        profits.update({count: net_value})
+        positive_color = {count: net_value}
+        print(colored(f'{positive_color}', 'green'))
+    return win_rate
