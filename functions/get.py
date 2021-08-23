7a3c5c678f6be04138bc534a5ee5012b67dbcb8b
Robert Roman <robertroman7@gmail.com>
2021-08-22 22:51:42 -0500

    major update

--- functions/get.py
+++ functions/get.py
@@ -8,6 +8,29 @@ from database.profit import profits
 from functions.webdriver import driver
 
 
+def net_all(long_stoploss_value, long_takeprofit_value, short_stoploss_value, short_takeprofit_value, wait):
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
+    try:
+        time.sleep(.5)
+        check = driver.find_elements_by_class_name("additional_percent_value")[0]
+        check.find_element_by_xpath('./span[contains(@class, "neg")]')
+        negative = True
+    except NoSuchElementException:
+        negative = False
+
+    if negative:
+        net_profit = driver.find_elements_by_class_name("additional_percent_value")[0].text.split(" %")
+        net_value = -float(net_profit[0])
+        profits.update({-net_value: ["Long Stoploss:", long_stoploss_value, "Long Take Profit:", long_takeprofit_value, "Short Stoploss:", short_stoploss_value, "Short Take Profit:", short_takeprofit_value]})
+        print(colored(f'Net Profit: -{net_value}% --> Long Stoploss: {long_stoploss_value}, Long Take Profit: {long_takeprofit_value}, Short Stoploss: {short_stoploss_value}, Short Take Profit: {short_takeprofit_value}', 'red'))
+    else:
+        net_profit = driver.find_elements_by_class_name("additional_percent_value")[0].text.split(" %")
+        net_value = float(net_profit[0])
+        profits.update({net_value: ["Long Stoploss:", long_stoploss_value, "Long Take Profit:", long_takeprofit_value, "Short Stoploss:", short_stoploss_value, "Short Take Profit:", short_takeprofit_value]})
+        print(colored(f'Net Profit: {net_value}% --> Long Stoploss: {long_stoploss_value}, Long Take Profit: {long_takeprofit_value}, Short Stoploss: {short_stoploss_value}, Short Take Profit: {short_takeprofit_value}', 'green'))
+    return net_profit
+
+
 def net_both(stoploss_value, takeprofit_value, wait):
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
     try:
@@ -55,13 +78,13 @@ def net_profit_stoploss(count, wait):
 
 
 def net_profit_takeprofit(count, wait):
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
     try:
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "additional_percent_value")))
         time.sleep(.5)
         check = driver.find_elements_by_class_name("additional_percent_value")[0]
         check.find_element_by_xpath('./span[contains(@class, "neg")]')
         negative = True
-    except NoSuchElementException:
+    except (NoSuchElementException, IndexError):
         negative = False
 
     if negative:
