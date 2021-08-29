217fd4871a751bbc508673037439f4d5bf9d9a00
Robert Roman <robertroman7@gmail.com>
2021-08-28 22:18:16 -0500

    Changed TradeView To a GUI

---
+++ OptimizeLong.py
@@ -0,0 +1,103 @@
+from execute_script import Main
+import random
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from database.profit import profits
+from my_functions import Functions
+
+url = 'https://www.tradingview.com/chart/'
+
+
+class LongScript(Functions):
+
+    def __init__(self):
+        Main.__init__(self)
+        self.driver = self.create_driver()
+        self.run_script()
+
+    def run_script(self):
+        """find the best stop loss value."""
+
+        # Loading Webpage
+        wait = WebDriverWait(self.driver, 5)
+        self.driver.get(url)
+        time.sleep(1)
+        self.click_strategy_tester()
+        try:
+            self.click_overview()
+        except NoSuchElementException:
+            time.sleep(1)
+            self.click_overview()
+        print("Generating Max Profit For Stop Loss.")
+        print("Loading script...\n")
+        self.click_settings_button(wait)
+        self.click_input_tab()
+        self.click_enable_long_strategy_checkbox()
+        self.click_rest_all_inputs()
+        self.click_ok_button()
+
+        # Searching for best stop loss and take profit for your strategy.
+        count = 0
+        while count < int(self.maxAttemptsValue.text()):
+            count += 1
+            stoploss_value = round(
+                random.uniform(float(self.minLongStoplossValue.text()), float(self.maxLongStoplossValue.text())),
+                int(self.decimalPlaceValue.text()))
+            takeprofit_value = round(
+                random.uniform(float(self.minLongTakeprofitValue.text()), float(self.maxLongTakeprofitValue.text())),
+                int(self.decimalPlaceValue.text()))
+            try:
+                # click settings button
+                self.click_settings_button(wait)
+
+                # click both input boxes and add values.
+                self.click_long_inputs(stoploss_value, takeprofit_value, wait)
+
+                # extract the net profit percentage.
+                self.get_net_both(stoploss_value, takeprofit_value, wait)
+
+            except (StaleElementReferenceException, TimeoutException, NoSuchElementException):
+                print("script has timed out.")
+                break
+
+        # adding new take profit to your strategy.
+        self.click_settings_button(wait)
+        best_key = self.find_best_key_both()
+        self.click_long_inputs(profits[best_key][1], profits[best_key][3], wait)
+        time.sleep(.5)
+
+        print("\n----------Results----------\n")
+        self.click_overview()
+        self.print_best_both()
+        self.click_performance_summary()
+        self.print_total_closed_trades()
+        self.print_net_profit()
+        self.print_win_rate()
+        self.print_max_drawdown()
+        self.print_sharpe_ratio()
+        self.print_sortino_ratio()
+        self.print_win_loss_ratio()
+        self.print_avg_win_trade()
+        self.print_avg_loss_trade()
+        self.print_avg_bars_in_winning_trades()
+        # print("\n----------More Results----------\n")
+        # self.print_gross_profit()
+        # self.print_gross_loss()
+        # self.print_buy_and_hold_return()
+        # self.print_max_contracts_held()
+        # self.print_open_pl()
+        # self.print_commission_paid()
+        # self.print_total_open_trades()
+        # self.print_number_winning_trades()
+        # self.print_number_losing_trades()
+        # self.print_percent_profitable()
+        # self.print_avg_trade()
+        # self.print_avg_win_trade()
+        # self.print_avg_loss_trade()
+        # self.print_largest_winning_trade()
+        # self.print_largest_losing_trade()
+        # self.print_avg_bars_in_trades()
+        # self.print_avg_bars_in_winning_trades()
+        # self.print_avg_bars_in_losing_trades()
+        # self.print_margin_calls()
---
+++ OptimizeLongTakeprofit.py
@@ -0,0 +1,90 @@
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import numpy as np
+from execute_script import Main
+from my_functions import Functions
+
+url = 'https://www.tradingview.com/chart/'
+
+
+class LongTakeProfit(Functions):
+
+    def __init__(self):
+        Main.__init__(self)
+        self.driver = self.create_driver()
+        self.run_script()
+
+    def run_script(self):
+        """find the best take profit value."""
+
+        # Loading Webpage.
+        my_range = np.arange(float(self.minLongTakeprofitValue.text()), float(self.maxLongTakeprofitValue.text()), float(self.LongIncrementValue.text()))
+        wait = WebDriverWait(self.driver, 10)
+        self.driver.get(url)
+        self.click_strategy_tester()
+        try:
+            self.click_overview()
+        except NoSuchElementException:
+            time.sleep(1)
+            self.click_overview()
+        print("Generating Max Profit For Take Profit.")
+        print("Loading script...\n")
+        self.click_settings_button(wait)
+        self.click_input_tab()
+        self.click_enable_long_strategy_checkbox()
+        self.click_rest_all_inputs()
+        self.click_ok_button()
+
+        # Searching for best take profit for your strategy.
+        for number in my_range:
+            count = round(number, 2)
+            try:
+                self.click_settings_button(wait)
+                self.click_long_takeprofit_input(count, wait)
+                self.get_net_profit_takeprofit(count, wait)
+            except (StaleElementReferenceException, TimeoutException, NoSuchElementException):
+                print("script has timed out.")
+                break
+
+        # adding the best take profit to your strategy on TradingView.
+        self.click_settings_button(wait)
+        best_key = self.find_best_takeprofit()
+        self.click_long_takeprofit_input(best_key, wait)
+        time.sleep(1)
+
+        # Printing Results of the best take profit value found.
+        print("\n----------Results----------\n")
+        self.click_overview()
+        self.print_best_takeprofit()
+        self.click_performance_summary()
+        self.print_total_closed_trades()
+        self.print_win_rate()
+        self.print_net_profit()
+        self.print_max_drawdown()
+        self.print_sharpe_ratio()
+        self.print_sortino_ratio()
+        self.print_win_loss_ratio()
+        self.print_avg_win_trade()
+        self.print_avg_loss_trade()
+        self.print_avg_bars_in_winning_trades()
+        # print("\n----------More Results----------\n")
+        # self.print_gross_profit()
+        # self.print_gross_loss()
+        # self.print_buy_and_hold_return()
+        # self.print_max_contracts_held()
+        # self.print_open_pl()
+        # self.print_commission_paid()
+        # self.print_total_open_trades()
+        # self.print_number_winning_trades()
+        # self.print_number_losing_trades()
+        # self.print_percent_profitable()
+        # self.print_avg_trade()
+        # self.print_avg_win_trade()
+        # self.print_avg_loss_trade()
+        # self.print_largest_winning_trade()
+        # self.print_largest_losing_trade()
+        # self.print_avg_bars_in_trades()
+        # self.print_avg_bars_in_winning_trades()
+        # self.print_avg_bars_in_losing_trades()
+        # self.print_margin_calls()
