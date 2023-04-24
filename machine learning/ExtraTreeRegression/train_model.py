caff80ec1a2ad911132f182ca3d65166dda956fa
Robert Roman <robertroman7@gmail.com>
2023-04-24 17:06:08 -0500

    Updated parameter search

--- machine learning/ExtraTreeRegression/train_model.py
+++ machine learning/ExtraTreeRegression/train_model.py
@@ -69,12 +69,12 @@ if __name__ == "__main__":
 
     # Define a grid of parameters to search for the best combination
     param_grid = {
-        'n_estimators': [50, 100, 200, 300, 500],
-        'max_depth': [None, 10, 20, 30, 40, 50],
-        'min_samples_split': [2, 5, 10, 15, 20],
-        'min_samples_leaf': [1, 2, 4, 6, 8],
+        'n_estimators': [100, 200, 300, 500, 1000, 1500],
+        'max_depth': list(range(5, 51, 5)) + [None],
+        'min_samples_split': [2, 5, 10, 15, 20, 30, 40],
+        'min_samples_leaf': [1, 2, 3, 4, 5, 6, 8, 10, 12],
         'bootstrap': [True, False],
-        'max_features': [1.0, 'sqrt', 'log2']
+        'max_features': [1.0, 'sqrt', 'log2', 0.5, 0.75]
     }
 
     # Perform GridSearchCV to find the best parameters
